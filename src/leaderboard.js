1ec496bfcb71736337c03148b4247c0ce57cad04
Vimuth <62249192+VimuthM@users.noreply.github.com>
2020-12-16 01:26:24 +0800

    Create leaderboard.js

---
+++ src/leaderboard.js
@@ -0,0 +1,22 @@
+// Leaderboard
+// ---------------------------------------
+function getLeaderboardRow(userID) {
+  var leaderboard_sheet = SpreadsheetApp.openById(sheet_id).getSheetByName("Normal_Leaderboard");
+  var rangeData = leaderboard_sheet.getDataRange();
+  var lastRow = rangeData.getLastRow();
+  var lastColumn = rangeData.getLastColumn();
+  var searchRange = leaderboard_sheet.getRange(2, 1, lastRow - 1, lastColumn);
+  var rangeValues = searchRange.getValues();
+  var result = "Leaderboards" + '\n';
+  
+  for (i = 0; i < 3; i++) {
+      var name = rangeValues[i][0];
+      var room = rangeValues[i][1];
+      var totalCredits = rangeValues[i][2];
+      result = result + (i+1).toString() + ". " + name + " (" + room + ") " +  ": " + totalCredits + " Credits" + '\n';
+  } 
+  return result;
+ 
+}
+// ---------------------------------------
+   
