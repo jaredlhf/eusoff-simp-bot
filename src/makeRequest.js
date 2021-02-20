be691e88b61133714fbca4bdb4b1b966d04e4d16
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2021-02-20 18:43:43 +0800

    Update makeRequest so it works with new DateTime (#17)
    * Update makeRequest so it works with new DateTime
    
    now[0] -> now.ewDate
    now[1] -> now.ewTime
    
    * moved ew to here

--- src/makeRequest.js
+++ src/makeRequest.js
@@ -84,7 +84,7 @@ function makeRequest(userID, data) {
     var request = category_number_remark.split(' ')[0];
     var deducted_credit = category_number_remark.split(' ')[1];
     var new_credits = parseInt(total_credits) - parseInt(deducted_credit);
-    var now = currentDateTime();
-    newRequest(new_ref, request, deducted_credit, userID, "Available", now[0], now[1], "-", 0, "", "");
+    var now = getDateTime();
+    newRequest(new_ref, request, deducted_credit, userID, "Available", now.date + 'Ew', now.time + 'Ew', "-", 0, "", "");
     setUserCredits(userID, new_credits);
 }
