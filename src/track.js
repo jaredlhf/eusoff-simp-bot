fe9129795e895a482f904fa6ead7ea58ac3045c6
gordonlzy <61217117+gordonlzy@users.noreply.github.com>
2021-02-21 02:43:16 +0800

    Add track.js (#19)
    * Add track.js
    
    The seeds of the anti-cheat system
    
    * The anti-cheat shoot :)
    
    Co-authored-by: Vimuth <62249192+VimuthM@users.noreply.github.com>

---
+++ src/track.js
@@ -0,0 +1,28 @@
+var Track = SpreadsheetApp.openById(sheet_id).getSheetByName('Track');
+
+var trackRangeData = Track.getDataRange();
+var trackLastColumn = trackRangeData.getLastColumn();
+var trackLastRow = trackRangeData.getLastRow();
+var trackSearchRange = Track.getRange(2, 1, trackLastRow - 1, trackLastColumn);
+var trackRangeValues = trackSearchRange.getValues();
+
+var cap = 5;
+
+function addUserToTrack(userId) {
+  var next = userLastRow();
+  Track.appendRow([userId]);
+  var temp = Track.getRange(trackLastRow + 1, 1).getValues();
+  Track.getRange(1, next).setValue(temp);
+}
+
+function calculateCaps() {
+  var num = userLastRow();
+  for (i = 2; i <= num; i++) {
+    var sum = 0;
+    for (j = 2; j <= num; j++) {
+      var n = Track.getRange(i, j).getValues()[0][0];
+      sum += (n > cap) ? 5 : n;
+    }
+    Users.getRange(i, 9).setValue(sum);
+  }
+}
