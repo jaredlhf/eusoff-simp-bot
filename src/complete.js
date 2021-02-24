168fd8194371aec167857b5fadcc391787bf7922
Jared Lim <71449770+jaredlhf@users.noreply.github.com>
2021-02-24 16:10:46 +0800

    Uodated strings and added ref number to selection
    added message reply to users and slaves upon completion of task (simp and normal), added ref id in selection of task to complete

--- src/complete.js
+++ src/complete.js
@@ -9,7 +9,7 @@ function viewOwnTaken(userID) {
     if (req.userId === userID && req.status === "Taken") {
         keyboard[count] = [
             {
-              text: req.request + " (" + req.credits + " cr), " + req.time.slice(0, -2) + ", " + req.date.slice(0, -2),
+              text: req.ref + '. ' + req.request + " (" + req.credits + " cr), " + req.time.slice(0, -2) + ", " + req.date.slice(0, -2),
               callback_data: 'complete-' + i,
             },
         ];
@@ -32,17 +32,26 @@ function completeRequest(userID, data) {
     var slave = userInfo(req.slaveId);
     var pending = parseInt(req.pending);
     var new_credits = parseInt(slave.total_credits) + pending;
+    var user = userInfo(userID);
 
     if (pending === 0) {      
       setUserSimpCount(slave.userId, slave.simp_count + 1);
+      setRequestStatus(refId, "Completed");
+      setRequestPending(refId, 0);
+      setUserCredits(req.slaveId, new_credits);
+      inc(locateUserRow(req.slaveId), locateUserRow(userID));
+
+      sendText(userID, 'You have confirmed the completion: '.bold() + '\nRequest: ' + req.request + '\nSimped by: ' + slave.name + ' (' + slave.room + ') ' + '\nRef number: ' + req.ref + '\nRemark: ' + req.remark);
+      sendText(slave.userId, 'Thank you for simping for ' + user.name + '\nRequest: ' + req.request + '\nRef number: ' + req.ref + '\nRemark: ' + req.remark);
+    } else {
+      setRequestStatus(refId, "Completed");
+      setRequestPending(refId, 0);
+      setUserCredits(req.slaveId, new_credits);
+      inc(locateUserRow(req.slaveId), locateUserRow(userID));
+
+      sendText(userID, 'You have confirmed the completion: '.bold() + '\nRequest: ' + req.request + '\nTaken by: ' + slave.name + ' (' + slave.room + ') \nCredit(s): ' + req.credits + '\nRef number: ' + req.ref + '\nRemark: ' + req.remark);
+      sendText(slave.userId, 'Thank you for helping ' + user.name + "\nYou've received " + req.credits + ' credit(s)' + '\nYou currently have ' + user.total_credits + ' credit(s)');
     }
-
-    setRequestStatus(refId, "Completed");
-    setRequestPending(refId, 0);
-    setUserCredits(req.slaveId, new_credits);
-    inc(locateUserRow(req.slaveId), locateUserRow(userID));
-
-    sendText(userID, 'Request complete');
 }
 
 function locateUserRow(userId) {
